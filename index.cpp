9db28cf4191fab630706044342592914fce7ef26
Davi Belo <davibelocontato@gmail.com>
2022-07-07 20:18:22 -0300

    first commit

---
+++ index.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num;
+    cin>>num;
+    
+    if(num%7==0){
+        cout<<"É MULTIPLO DE 7"<<endl;
+    }else{
+        cout<<"ESSE NÃO É"<<endl;
+    }
+    return 0;
+}
